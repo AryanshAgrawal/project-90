708c362e5a1f63dcf96f050e4a7d07c78d00fb6f
AryanshAgrawal <76209119+AryanshAgrawal@users.noreply.github.com>
2021-09-15 18:24:09 +0530

    Add files via upload

---
+++ game_login_page.js
@@ -0,0 +1,7 @@
+function login(){
+    player1_name = document.getElementById("player-nameinput1").value;
+    player2_name = document.getElementById("player-nameinput2").value;
+    localStorage.setItem("player1_name" , player1_name);
+    localStorage.setItem("player2_name" , player2_name);
+    window.location.replace="game_quiz_page.html";
+    }
